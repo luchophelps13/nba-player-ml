c53836c9afc8f90f606d7270fc7c0ed6172b93cd
Lucas Phillips <53153598+luchophelps13@users.noreply.github.com>
2021-02-27 20:02:29 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleDiv() {
-    var x = document.getElementById('graph');
-    if ( window.getComputedStyle(x, null).getPropertyValue("display") === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
